99f80bc0c2ec9aae88f6850b0392227ba4f842f5
Suji100418 <klop100418@gmail.com>
2023-11-29 21:50:32 +0900

    Init UnrealProject

---
+++ KawaiPattern/Source/KawaiPattern/KawaiPatternGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KawaiPatternGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KAWAIPATTERN_API AKawaiPatternGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
